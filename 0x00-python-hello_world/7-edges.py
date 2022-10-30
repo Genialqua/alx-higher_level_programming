c7dceeb819e7a74c98a23c958a226c6e1d09d879
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2022-10-30 22:35:43 +0100

    String copy-cut-paste print formatting

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[0:3]
+word_last_2 = word[7:]
+middle_word = word[1:8]
+print(f"First 3 letters: {word_first_3}".format(word[0:3]))
+print(f"Last 2 letters: {word_last_2}".format(word[7:]))
+print(f"Middle word: {middle_word}".format(word[1:8]))
