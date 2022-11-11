174e897c5d4a011e9c591cc27edbdacc418dd463
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2022-11-11 11:14:36 +0100

    Picking last digits of random numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,9 +8,12 @@ if number >= 0:
 else:
     last_digit = number % -10
 
+la_di = "Last digit of"
+la_di1 = "and is less than 6 and not 0"
+
 if last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
+    print(la_di+" {} is {} and is greater than 5".format(number, last_digit))
 elif last_digit == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_digit))
+    print(la_di+" {} is {} and is 0".format(number, last_digit))
 elif (last_digit < 6) and (last_digit != 0):
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
+    print(la_di+" {} is {} ".format(number, last_digit) + la_di1)
