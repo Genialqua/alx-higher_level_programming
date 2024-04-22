d351cff34f63449b2386dfe8f9c1f647dc558e16
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-04-22 15:28:15 +0100

    A script that prints a message depending of the number of arguments passed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,8 +3,10 @@
 // A script for arguments
 const { argv } = require('process');
 
-if (argv.length <= 3) {
+if (argv.length < 3) {
   console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
