8386c8736fa950733b140f927b9a8676bbaffc15
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-05-14 07:33:03 +0100

    A script to compute and print factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,10 +4,17 @@ const { argv } = require('process');
 const a = parseFloat(argv[2]);
 
 function factorial (a) {
-  if (a === 0 || a === 1) {
+  if (isNaN(a) || a < 0) {
+    return 1;
+  } else if (a === 0) {
     return 1;
   } else {
-    return a * factorial(a - 1);
+    let result = 1;
+    for (let i = 1; i <= a; i++) {
+      result *= i;
+    }
+    return result;
   }
 }
+
 console.log(factorial(a));
