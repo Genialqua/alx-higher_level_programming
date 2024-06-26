e85afe9c0373b916ac2d61d52a09d7255e35c001
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-06-26 10:31:01 +0100

    A function to print result of division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = int(a) / int(b)
-        print("{:d} / {:d} = {:.1f}".format(a, b, result))
-        return (result)
-    except ZeroDivisionError:
-        print("Error: Division by zero")
-        return (None)
+        result = a / b
+    except:
+        result = None
     finally:
-        print("Inside finally block:")
-        print("Inside result: {:.1f}".format(result))
+        print("Inside result: {}".format(result))
+        return (result)
