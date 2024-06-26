ed11a4e4b328d89624f56fecaaa701d9df385375
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-06-26 10:39:26 +0100

    A function to print result of division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,10 @@
 def safe_print_division(a, b):
     try:
         result = a / b
+	return (result)
     except:
         result = None
+	return (None)
     finally:
         print("Inside result: {}".format(result))
         return (result)
