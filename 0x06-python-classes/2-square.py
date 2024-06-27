f8422fe360cfdcf574f94191f81da5368bad6ff4
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-06-27 16:49:30 +0100

    Creating a class with a private attribute and exception ability

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-""" A class Square that defines a square by: (based on 1-square.py)"""
 
 
 class Square:
-    """A class that defines a square"""
+    """ A class that defines a square by its size
+    """
     def __init__(self, size=0):
-        try:
-            if isinstance(size, int):
-                if size >= 0:
-                    self.__size = size
-                else:
-                    raise ValueError("size must be >= 0")
-            else:
-                raise TypeError("size must be an integer")
-        except TypeError as e:
-            print(e)
+        """ Method to initialize the square object
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
