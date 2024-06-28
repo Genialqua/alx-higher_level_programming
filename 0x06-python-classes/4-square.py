9310821f6f30cb06da15ce3b35613b0e56cd31ab
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-06-28 10:15:32 +0100

    Creating a class with a private attribute to change size

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,20 +6,11 @@ class Square:
     """A class that defines a square"""
 
     def __init__(self, size=0):
-        try:
-            if isinstance(size, int):
-                if size >= 0:
-                    self.__size = size
-                else:
-                    raise ValueError("size must be >= 0")
-            else:
-                raise TypeError("size must be an integer")
-        except (TypeError, ValueError) as e:
-            print(e)
+        self.size = size
 
     def area(self):
         """Calculate the area of the square"""
-        return self.__size ** 2
+        return self.size ** 2
 
     def get_size(self):
         """Getter method to retrieve the size of the square"""
